608a3d41248c20dbe7ae2f51cb0ee2fa64f90054
Hiroe Kato <hiroekato@ootsukaruinoMacBook-Air.local>
2024-01-17 14:32:47 +0900

    youtube見てやってみました。

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ $(function(){
         let accesstop = $(".access").offset().top;
 
 
-        if(scroll>(accesstop-200)){
-            if(scroll>4250){
+        if(scroll>(accesstop-300)){
+            if(scroll>5000){
                 $(".accessback").fadeOut(600);
             }else{
                 $(".accessback").fadeIn(600);
@@ -39,27 +39,39 @@ $(function(){
     });
 });
 
+// $(function(){
+//     $(window).on('scroll',function(){
+//         let scroll = $(window).scrollTop();
+
+//         console.log(scroll);
+//         let gallerytop = $(".gellery").offset().top;
+
+//         if(scroll>(gallerytop)){
+//             if(scroll>3000){
+//                 $(".no1").fadeOut(600);
+//             }else{
+//                 $(".no1").fadeIn(600);
+//             }
+        
+//         }else{
+//             $(".no1").fadeOut(600);
+//         }
+
+        
+//     });
+// });
+
+
+
 $(function(){
-    $(window).on('scroll',function(){
-        let scroll = $(window).scrollTop();
-
-        console.log(scroll);
-        let gallerytop = $(".gellery").offset().top;
-
-        if(scroll>(gallerytop)){
-            if(scroll>3000){
-                $(".no1").fadeOut(600);
-            }else{
-                $(".no1").fadeIn(600);
-            }
-        
-        }else{
-            $(".no1").fadeOut(600);
-        }
-
-        
+    $(window).scroll(function(){
+            $(`.fadein`).each(function(){
+                var trangetElement = $(this).offset().top;
+                var scroll = $(window).scrollTop();
+                var windowHeight = $(window).height();
+                if(scroll > trangetElement - windowHeight + 150){
+                    $(this).addClass(`scrollin`);
+                }
+            });
+        });
     });
-});
-
-
-
