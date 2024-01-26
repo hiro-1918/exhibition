74aaf02878c530f63ab27e67c71500ac519c6fd7
Hiroe Kato <hiroekato@ootsukaruinoMacBook-Air.local>
2024-01-26 15:30:22 +0900

    サイドバー追加

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(function(){
         
         console.log(scroll);
         let accesstop = $(".access").offset().top;
-
-
+        
+        
         if(scroll>(accesstop-200)){
             if(scroll>6700){
                 $(".accessback").fadeOut(600);
@@ -16,17 +16,49 @@ $(function(){
         }else{
             $(".accessback").fadeOut(600);
         }
-
-        // let visuwidth = $(".mainvisual").offset().width;
-        // if(scroll>50){
-        //     if(scroll>1000){
-        //         $(".mainvisual").each(function(){
-        //         visuwidth + scroll/3}
-        //         )};
+        
+        
+        $('.mainvisual img').css({
+            width:100/3+(scroll/10)+"%"    
+        });
+        
+        
+        $('.side').css({
+            top:(scroll+630)+"px"
+        });
+        
+        if(scroll>900){
+            if(scroll>5200){
+                $('.side').fadeOut(400);
+            }else{
+                $('.side').addClass('side2');
+                $('.side').fadeIn(400);
+            }
+        }else{
+            $('.side').fadeOut(400);
+        };
+        
+        //     ↑35行目　効いてない？
+        // if(scroll>900){
+        //     if(scroll>5200){
+        //         $('.side').removeClass('side2');
+        //     }else{
+        //         $('.side').addClass('side2');
+        //     }
+        // }else{
+        //     $('.side').removeClass('side2');
         // };
 
-
+        
+        
     });
+    $('.mainvisual img').css({
+        width:100/3+"%"
+    });
+    
+    
+    
+    
 });
 
 // $(function(){
@@ -42,12 +74,12 @@ $(function(){
 //             }else{
 //                 $(".no1").fadeIn(600);
 //             }
-        
+
 //         }else{
 //             $(".no1").fadeOut(600);
 //         }
 
-        
+
 //     });
 // });
 
@@ -55,21 +87,14 @@ $(function(){
 
 $(function(){
     $(window).scroll(function(){
-            $(`.fadein`).each(function(){
-                var trangetElement = $(this).offset().top;
-                var scroll = $(window).scrollTop();
-                var windowHeight = $(window).height();
-                if(scroll > trangetElement - windowHeight + 150){
-                    $(this).addClass(`scrollin`);
-                }
-            });
+        $(`.fadein`).each(function(){
+            var trangetElement = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if(scroll > trangetElement - windowHeight + 150){
+                $(this).addClass(`scrollin`);
+            }
         });
     });
-
-$(function(){
-    $(window).scroll(function(){
-        $(".mainvisual").each(function(){
-            if(scroll >)
-        });
-    })
 });
+
