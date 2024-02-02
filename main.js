32f11b988210ebfe1ca1a4161ae4b45ed8b12ee7
Hiroe Kato <hiroekato@ootsukaruinoMacBook-Air.local>
2024-02-02 16:48:39 +0900

    メディアクエリ作成

--- main.js
+++ main.js
@@ -3,27 +3,27 @@ $(function(){
         let scroll = $(window).scrollTop();
         
         $('.header').css({
-        top:(scroll+10)+"px"
+            top:(scroll+10)+"px"
         });
-
+        
         if(scroll>650){
             $('header').fadeIn(600);
         }else{
             $('header').fadeOut(600);
         };
-
-
+        
+        
         $('.menu-culumn').css({
             top:(scroll-20)+"px"
-            });
-
-
+        });
+        
+        
         console.log(scroll);
         let accesstop = $(".access").offset().top;
         
         
         if(scroll>(accesstop-200)){
-            if(scroll>6700){
+            if(scroll>6500){
                 $(".accessback").fadeOut(600);
             }else{
                 $(".accessback").fadeIn(600);
@@ -33,10 +33,22 @@ $(function(){
             $(".accessback").fadeOut(600);
         }
         
+        // $('.mainvisual img').css({
+        //     width:100/3+(scroll/10)+"%"
+        // });
         
-        $('.mainvisual img').css({
-            width:100/3+(scroll/10)+"%"    
-        });
+
+        var wid = $(window).width();   
+
+        if(wid < 700){
+            $('.mainvisual img').css({
+                width:100-(scroll/10)+"%"
+            });
+        }else{
+            $('.mainvisual img').css({
+                width:100/3+(scroll/10)+"%"
+            });
+        };
         
         
         $(`.fadein`).each(function(){
@@ -66,23 +78,25 @@ $(function(){
             $('.side').fadeOut(400);
         };
         
-        //     ↑35行目　効いてない？
-        // if(scroll>900){
-        //     if(scroll>5200){
-        //         $('.side').removeClass('side2');
-        //     }else{
-        //         $('.side').addClass('side2');
-        //     }
-        // }else{
-        //     $('.side').removeClass('side2');
-        // };
-        
     });
     
+
+    var wid = $(window).width();   
+
+    if(wid < 700){
+        $('.mainvisual img').css({
+            width:100-(scroll/10)+"%"
+        });
+    }else{
+        $('.mainvisual img').css({
+            width:100/3+(scroll/10)+"%"
+        });
+    };
     
-    $('.mainvisual img').css({
-        width:100/3+"%"
-    });
+    
+    // $('.mainvisual img').css({
+    //     width:100/3+"%"
+    // });
     
     
     $('.menu').click(function(){
@@ -90,32 +104,32 @@ $(function(){
         if(line1.hasClass('line1after')){
             line1.removeClass('line1after');
         }else{
-        line1.addClass('line1after');
+            line1.addClass('line1after');
         };
         var line2 = $('.line2') ;
         if(line2.hasClass('line2after')){
             line2.removeClass('line2after');
         }else{
-        line2.addClass('line2after');
+            line2.addClass('line2after');
         };
         var line3 = $('.line3') ;
         if(line3.hasClass('line3after')){
             line3.removeClass('line3after');
         }else{
-        line3.addClass('line3after');
+            line3.addClass('line3after');
         };
-
+        
         // $('.height').css({
         //     top:5+"px"
         // })
-
+        
         var height = $('.height');
         if(height.hasClass('heightafter')){
             height.removeClass('heightafter');
         }else{
             height.addClass('heightafter');
         };
-
+        
         var culumn = $('.menu-culumn');
         if(culumn.hasClass('open')){
             culumn.removeClass('open');
@@ -125,10 +139,10 @@ $(function(){
             culumn.slideDown();
         }
     });
-
-
-
-
+    
+    
+    
+    
     $('#contact1').hover(
         function(){
             $('.arrow').addClass('rightfade');
@@ -141,23 +155,21 @@ $(function(){
             $('#contact1').removeClass('gray');
         });
         
-    
-    $('#contact2').hover(
-        function(){
-            $('.arrow2').addClass('rightfade');
-            // $('.arrow2sub').addClass('rightfade').addClass('transform');
-            $('#contact2').addClass('gray');
-        },
-        function(){
-            $('.arrow2').removeClass('rightfade');
-            // $('.arrow2sub').removeClass('rightfade').removeClass('transform');
-            $('#contact2').removeClass('gray');
+        
+        $('#contact2').hover(
+            function(){
+                $('.arrow2').addClass('rightfade');
+                // $('.arrow2sub').addClass('rightfade').addClass('transform');
+                $('#contact2').addClass('gray');
+            },
+            function(){
+                $('.arrow2').removeClass('rightfade');
+                // $('.arrow2sub').removeClass('rightfade').removeClass('transform');
+                $('#contact2').removeClass('gray');
+            });
         });
-
-
-    });
         
-        
+
         
         
         
